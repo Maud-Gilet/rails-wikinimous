b565be45ab1285c59b227144108f0b67d63ea394
Maud Gilet <maud.gilet@gmail.com>
2018-11-20 15:40:44 +0100

    model and controller created for article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+seed_array = []
+
+10.times do
+  h = {
+    title: Faker::HowIMetYourMother.quote,
+    content: Faker::Food.description
+  }
+  seed_array << h
+end
+
+seed_array.each do |item|
+  Article.create(item)
+end
